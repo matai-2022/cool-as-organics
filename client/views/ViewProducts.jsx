767d9378ab5af07d5a36a28c4fe5d446a870f7fe
Mano <m.raankin@gmail.com>
2022-07-15 13:59:27 +1200

    added border styling

--- client/views/ViewProducts.jsx
+++ client/views/ViewProducts.jsx
@@ -23,9 +23,9 @@ function ViewProducts() {
   }, [])
 
   return (
-    <table>
-      <thead>
-        <tr>
+    <table className="border-2">
+      <thead className="border-2">
+        <tr className="divide-y-2">
           <th>Product</th>
           <th>Open date</th>
           <th>Expiry date</th>
@@ -33,9 +33,9 @@ function ViewProducts() {
           <th>Product type</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className=" divide-y-2">
         {list.map((item) => (
-          <tr key={item.id}>
+          <tr className=" divide-x-2" key={item.id}>
             <td>{item.name} </td>
             <td>{moment(item.openDate).format('ddd D MMM YYYY')}</td>
             <td>{moment(item.expiryDate).format('ddd D MMM YYYY')}</td>
