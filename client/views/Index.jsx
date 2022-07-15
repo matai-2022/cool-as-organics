0346034dddbb2f1690e845452bc427ba06b50883
Docie Ng <docieng@Docies-MacBook-Air.local>
2022-07-15 14:58:46 +1200

    complete code for index

--- client/views/Index.jsx
+++ client/views/Index.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {fetchProducts} from '../../client/apis/products'
 
 function Index() {
-  return <></>
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+    .then(products => {
+      setProducts(products)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }, [])
+  return (
+    <>
+    <div className="tailwind">
+      <p className="some font">Products close to expiry!</p>
+    </div>
+
+    <div> 
+      {products.map((product) => {
+        console.log(product) 
+        return <p key={product.id}>{product.name} {product.expiryDate}</p>
+      })}  
+    </div>
+    </>
+  )
 }
 
 export default Index
