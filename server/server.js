0d8affbbb7ef91742ca2457f59232c92c2f7cef2
Phuong Tran <ptranny@gmail.com>
2022-07-16 13:08:20 +1200

    Resolve merge conflict with server.js

--- server/server.js
+++ server/server.js
@@ -2,11 +2,8 @@ const express = require('express')
 const path = require('path')
 
 const productRoutes = require('./routes/products')
-<<<<<<< HEAD
 const productTypeRoutes = require('./routes/productTypes')
-=======
 const recipeRoutes = require('./routes/recipes')
->>>>>>> main
 
 const server = express()
 
@@ -14,12 +11,9 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/products', productRoutes)
-<<<<<<< HEAD
 server.use('/api/v1/productTypes', productTypeRoutes)
 
-=======
 server.use('/api/v1/recipes', recipeRoutes)
->>>>>>> main
 server.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
