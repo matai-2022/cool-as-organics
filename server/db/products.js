f5e8f3a488b6e959adc4e1596482a1014d002ff1
Phu <ptranny@gmail.com>
2022-07-19 10:52:58 +1200

    Expose PATCH /api/v1/products and GET /api/v1/products/all

--- server/db/products.js
+++ server/db/products.js
@@ -1,5 +1,19 @@
 const connection = require('./connection')
 
+function getAllProducts(db = connection) {
+  return db('products')
+    .leftJoin('product_types', 'product_types.id', 'products.product_type_id')
+    .select(
+      'products.id as id',
+      'products.name as name',
+      'open_date as openDate',
+      'expiry_date as expiryDate',
+      'status',
+      'compartment',
+      'product_types.name as productType'
+    )
+}
+
 function getOpenProducts(db = connection) {
   return db('products')
     .leftJoin('product_types', 'product_types.id', 'products.product_type_id')
@@ -19,4 +33,8 @@ function addProduct(product, db = connection) {
   return db('products').insert(product)
 }
 
-module.exports = { getOpenProducts, addProduct }
+function updateProduct(id, product, db = connection) {
+  return db('products').where('id', id).update(product)
+}
+
+module.exports = { getAllProducts, getOpenProducts, addProduct, updateProduct }
--- server/routes/products.js
+++ server/routes/products.js
@@ -4,6 +4,16 @@ const db = require('../db/products')
 
 const router = express.Router()
 
+router.get('/all', async (req, res) => {
+  try {
+    const products = await db.getAllProducts()
+    res.json(products)
+  } catch (error) {
+    console.error(error.message)
+    res.sendStatus(500)
+  }
+})
+
 router.get('/', async (req, res) => {
   try {
     const products = await db.getOpenProducts()
@@ -37,6 +47,34 @@ router.post('/', async (req, res) => {
     const id = await db.addProduct(product)
 
     res.json(id)
+  } catch (error) {
+    console.error(error.message)
+    res.sendStatus(500)
+  }
+})
+
+router.patch('/', async (req, res) => {
+  try {
+    const {
+      id,
+      name,
+      openDate: open_date,
+      expiryDate: expiry_date,
+      status,
+      compartment,
+      productTypeId: product_type_id,
+    } = req.body
+
+    await db.updateProduct(id, {
+      name,
+      open_date,
+      expiry_date,
+      status,
+      compartment,
+      product_type_id,
+    })
+
+    res.sendStatus(200)
   } catch (error) {
     console.error(error.message)
     res.sendStatus(500)
