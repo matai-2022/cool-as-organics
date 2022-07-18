b1ec332446ec3c2edd70967abbe62123c41a9a2a
Mano <m.raankin@gmail.com>
2022-07-18 13:16:57 +1200

    cannot toggle permissions

--- client/App.jsx
+++ client/App.jsx
@@ -6,8 +6,65 @@ import Index from './views/Index'
 import Recipes from './views/Recipes'
 import ViewProducts from './views/ViewProducts'
 import AddProduct from './views/AddProduct'
+// const cron = require('node-cron')
 
 function App() {
+  // const job = cron.schedule(
+  //   '*/30 * * * * *',
+  //   () => {
+  //     console.log('running a task every minute')
+  //     //notifyMe()
+  //   },
+  //   { schedule: false }
+  // )
+  //job.start()
+  let bool = false
+  function noteBool() {
+    bool = bool ? false : true
+    console.log(bool)
+    if (bool === false) {
+      //Notification.permission == 'default'
+    } else {
+      console.log(bool)
+      notifyMe()
+    }
+  }
+  function makeNotification() {
+    var notification = new Notification('This is a clickable notification', {
+      body: 'Click Me',
+    })
+
+    notification.onclick = function () {
+      window.open('http://localhost:3000/products/add')
+    }
+  }
+
+  function notifyMe() {
+    if (!('Notification' in window)) {
+      alert('This browser does not support desktop notification')
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === 'granted') {
+      // If it's okay let's create a notification
+      new Notification('Hi there granted!')
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission()
+        .then(function (permission) {
+          // If the user accepts, let's create a notification
+          if (permission === 'granted') {
+            // new Notification('Hi there aaa!')
+            makeNotification()
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
   return (
     <>
       <div className="app">
@@ -18,6 +75,9 @@ function App() {
           <Route path="/products/view" element={<ViewProducts />} />
           <Route path="/products/add" element={<AddProduct />} />
         </Routes>
+      </div>
+      <div>
+        <button onClick={noteBool}>Notify me!</button>
       </div>
     </>
   )
