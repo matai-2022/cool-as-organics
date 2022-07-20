acbb4442d813a9f5d4c87eff866e043ef101591b
Docie Ng <docieng@Docies-MacBook-Air.local>
2022-07-20 14:31:41 +1200

    Improve styling to addProduct so it matches the rest of the components

--- client/subcomponents/AddProductForm.jsx
+++ client/subcomponents/AddProductForm.jsx
@@ -17,38 +17,48 @@ function AddProductForm({initialValues, handleSubmit}) {
   }, [])
 
   return (
-    <Formik
+    <>
+      <h1 className='my-4 text-3xl text-center'>Add a product</h1>
+
+      <Formik
     initialValues={initialValues}
     onSubmit={handleSubmit}>
 
-  <div className='items-center md:shrink-0 w-full'>
-    <Form className='px-5 my-10 space-y-9 border-x-8 border-y-4 border-yellow-300 bg-yellow-50 rounded focus-yellow-400 drop-shadow-md'>
-      <h1 className='font-bold text-3xl text-center pt-10'>Add a product!</h1>
-        <label className='label block text-gray-600 mt-8'>
+    <div className='h-screen flex flex-col items-center bg-zinc-50'>
+
+    <Form className='w-11/12 border rounded border-zinc-200 bg-white mt-4 mb-4'>
+
+        <label className='label block pb-4 pt-2 ml-4'>
         Name
-        <Field name='name' type='text' className='border border-grey-600 block py-2 px-8 w-rounded w-1/2'/>
+        <Field name='name' type='text' 
+        className='border border-zinc-200 block py-2 px-8 w-2/3'/>
         </label>
 
-        <label className='label block text-gray-600'>
+        <label className='label block pb-4 ml-4'>
         Open Date
-          <Field name='openDate' type='date' className='border border-grey-600 block py-2 px-4 w-rounded w-full'/>
+          <Field name='openDate' type='date'
+          className='border border-zinc-200 block py-2 px-4 w-2/3'/>
         </label>
       
 
-      <label className='label block text-gray-600'>
+      <label className='label block pb-4 ml-4'>
         Expiry Date
-        <ExpiryDate name='expiryDate' type='date' className='border border-grey-600 block py-2 px-4 w-rounded w-full'/>
+        <ExpiryDate name='expiryDate' type='date'
+        className='border border-zinc-200 block py-2 px-4 w-2/3'/>
       </label>
       
 
-      <label className='label block text-gray-600'>
+      <label className='label block pb-4 ml-4'>
         Lifespan
-        <Lifespan name='lifespan' type='number' className='border border-grey-600 block py-2 px-4 w-rounded w-1/4'/>
+        <Lifespan name='lifespan' type='number' 
+        className='border border-zinc-200 block py-2 px-4 w-1/4'/>
       </label>
 
-      <label className='label block text-gray-600'>
+      <label className='label block pb-4 ml-4'>
         Product Type
-        <Field as='select' name='productTypeId' className='border border-grey-600 block py-2 px-4 w-rounded w-1/3'>
+        
+        <Field as='select' name='productTypeId' 
+        className='border border-zinc-200 block py-2 px-4 w-1/3'>
           <option value=''></option>
           {productTypes.map(
             productType => <option key={productType.id} value={productType.id}>{productType.name}</option>
@@ -56,21 +66,22 @@ function AddProductForm({initialValues, handleSubmit}) {
         </Field>
       </label>
       
-      <label className='label block text-gray-600'>
+      <label className='label block pb-4 ml-4'>
         Compartment
-        <Field as='select' name='compartment' className='border border-grey-600 block py-2 px-4 w-rounded w-1/3'>
+        <Field as='select' name='compartment' className='border border-zinc-200 block py-2 px-4 w-1/3'>
           <option value=''></option>
           <option value='fridge'>fridge</option>
           <option value='freezer'>freezer</option>
         </Field>
       </label>
       
-      <div className='pb-10 relative'>
-        <button type='submit' className='p-2 border border-yellow-500 rounded bg-white'>Add product</button>
+      <div className='relative'>
+        <button type='submit' className='p-2 border border-zinc-400 rounded bg-white mb-4 ml-4'>Add product</button>
       </div>
     </Form>
     </div>
   </Formik>
+  </>
   )
 }
 
@@ -119,4 +130,6 @@ function ExpiryDate(props) {
   }, [lifespan])
 
   return <input {...props} {...field} />
+
 }
+
