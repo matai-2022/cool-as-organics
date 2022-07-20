7aa6f6ef65b8c14f728ae05af1eb86ad56e6f1a3
Phu <ptranny@gmail.com>
2022-07-20 13:48:21 +1200

    add styling to stats component

--- client/views/Stats.jsx
+++ client/views/Stats.jsx
@@ -10,30 +10,42 @@ function Stats() {
     setTopPurchased(await getTopPurchasedProducts())
     setTopWasted(await getTopWastedProducts())
   }, [])
-
+  console.log(topWasted)
   return (
-    <>
-      <div>
-        <h2>Top purchases </h2>
-        <ul>
-          {topPurchased.map((product) => {
-            return <li key={product}>{product}</li>
-          })}
-        </ul>
-      </div>
-      <div>
-        <h2>Most wasted </h2>
-        <ul>
-          {topWasted.map((product) => {
+    <div>
+      <h1 className="mb-4 mt-4 text-center text-3xl"> Stats </h1>
+      <div className=" h-screen flex flex-col items-center bg-zinc-50 ">
+        <div className="w-11/12 border rounded border-zinc-200 bg-white mt-4 px-4 py-4">
+          <h2 className="text-xl font-medium mb-4 ">Top purchases </h2>
+          <ul>
+            {topPurchased.map((product, i) => {
+              return (
+                <li key={product} className="flex justify-start p-2 pl-4">
+                  <div className=" w-6 h-6 border-2 rounded-full flex items-center justify-center bg-zinc-200 ">
+                    {i + 1}
+                  </div>
+                  {product}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <ul className="w-11/12 border rounded border-zinc-200 bg-white mt-4 px-4 py-4">
+          <h2 className="text-xl font-medium mb-4 ">Top wasted </h2>
+          {topWasted.map((product, i) => {
             return (
-              <li key={product.name}>
+              <li key={product.name} className="flex justify-start p-2 pl-4">
+                <span className="w-6 h-6 border-2 rounded-full flex items-center justify-center bg-zinc-200 ">
+                  {i + 1}
+                </span>{' '}
                 {product.name} {`${product.wasted * 100} %`}
               </li>
             )
           })}
         </ul>
       </div>
-    </>
+    </div>
   )
 }
 
